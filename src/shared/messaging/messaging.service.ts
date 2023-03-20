20300aae4db8c8dac9459a200f5d81e7c079fedb
horusen <babacarkane@meltwater.org>
2023-03-20 11:44:40 +0000

    fix: Whatsapp messaging format

--- src/shared/messaging/messaging.service.ts
+++ src/shared/messaging/messaging.service.ts
@@ -19,20 +19,31 @@ export class MessagingService {
   }
 
   sendCreditTakenMessage(credit?: CreditLoan) {
-    const message = `Congratulations!\n\nYou've just taken *${credit?.amount}* GHC on credit from *Asante & Asante Limited*.\n\nDon't forget to pay before *${credit?.due_date}*.\n\nMessage sent by *kooza*`;
+    // const message = `Congratulations!\n\nYou've just taken *${credit?.amount}* GHC on credit from *Asante & Asante Limited*.\n\nDon't forget to pay before *${credit?.due_date}*.\n\nMessage sent by *kooza*`;
+    const message = `Hey *${
+      credit.customer.name
+    }*,\n\nCongrats on taking *${new Intl.NumberFormat('en-GH', {
+      style: 'currency',
+      currency: 'GHS',
+    }).format(credit.amount)}* on credit from ${
+      credit.business.name
+    }!\n\nWe're excited to have you as a customer and can't wait to help you achieve your goals. If you need any assistance, just let us know. \nThanks for choosing us and we look forward to serving you!\n\nMessage sent by *kooza*`;
 
     this.sendWhatsappMessage(message);
   }
 
   sendCreditPaidMessage(credit: CreditLoan) {
-    const message = `
-  Congratulations! you've payback the ${
-    credit.amount
-  } GHC credit you've taken from Asante & Asante Limited on ${Date.now()}.
-    
-  Thanks for your availability.
-    
-  Message sent by kooza`;
+    const message = `Hi *${
+      credit.customer.name
+    }*,\n\nCongratulations on paying off your *${new Intl.NumberFormat(
+      'en-GH',
+      {
+        style: 'currency',
+        currency: 'GHS',
+      },
+    ).format(credit.amount)}* debt to *${
+      credit.business.name
+    }*! We're thrilled to have you as a responsible and loyal customer, and we appreciate your trust and support. If you need any assistance or have any questions, just let us know. We're always here to help you. \n\nThanks for being awesome and we look forward to serving you in the future!\n\nMessage sent by *kooza*`;
 
     this.sendWhatsappMessage(message);
   }
